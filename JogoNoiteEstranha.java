afb7be6201c67a99b0dc290964f6e69e41c85bbb
RENATO DOS SANTOS <renato.santos25@senacsp.edu.br>
2025-05-23 20:39:24 -0300

    Atualizado o inicio do jogo e perguntas

--- JogoNoiteEstranha.java
+++ JogoNoiteEstranha.java
@@ -6,41 +6,30 @@ public class JogoNoiteEstranha {
     public String nome;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         JogoNoiteEstranha jogo = new JogoNoiteEstranha();
 
         while (true) {
+            System.out.print("""
+                                                                                                                                
+                               _____      _                     _                      _                              _         
+                              |     |   _| |___ ___ ___ ___ ___| |_ ___ ___    ___ ___| |_ ___ ___    _____ _ _ ___ _| |___ ___ 
+                              |  |  |  | . | -_|_ -| . | -_|  _|  _| .'|  _|  | -_|   |  _|  _| -_|  |     | | |   | . | . |_ -|
+                              |_____|  |___|___|___|  _|___|_| |_| |__,|_|    |___|_|_|_| |_| |___|  |_|_|_|___|_|_|___|___|___|
+                                                   |_|                                                                          
+                              """);
             System.out.println("\n=== MENU PRINCIPAL ===");
             System.out.println("1. Iniciar o jogo");
             System.out.println("2. Instruções");
             System.out.println("3. Sair");
             System.out.print("Escolha uma opção: ");
 
-            String escolha = scanner.nextLine();
+            String escolha = input.nextLine();
 
             switch (escolha) {
                 case "1":
-                    long inicio = System.nanoTime();
                     jogo.iniciarJogo();
-                    long fim = System.nanoTime();
-
-                    long duracao = fim - inicio;
-                    long segundosTotais = duracao / 1_000_000_000;
-                    long minutos = segundosTotais / 60;
-                    long segundosRestantes = segundosTotais % 60;
-
-                    if (minutos <= 0) {
-                        System.out.println("");
-                    } else if (minutos >= 1 && minutos <= 2) {
-                        System.out.println("Você demorou " + minutos + " minuto e " + segundosRestantes + " e segundos.");
-                        System.out.println("Você foi rápido e continua jovem!");
-                    } else if (minutos >= 2 && minutos <= 5) {
-                        System.out.println("Você demorou " + minutos + " minuto e " + segundosRestantes + " e segundos.");
-                        System.out.println("Você demorou um pouco e está adulto!");
-                    } else {
-                        System.out.println("Você demorou " + minutos + " minuto e " + segundosRestantes + " e segundos.");
-                        System.out.println("Você demorou muito e ficou muito velho!");
-                    }
+                    
                     break;
                 case "2":
                     System.out.println("\n=== INSTRUÇÕES ===");
@@ -49,7 +38,7 @@ public class JogoNoiteEstranha {
                     break;
                 case "3":
                     System.out.println("Saindo do jogo. Até a próxima!");
-                    scanner.close();
+                    input.close();
                     return;
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
@@ -58,18 +47,46 @@ public class JogoNoiteEstranha {
     }
 
     public void iniciarJogo() {
-        Scanner scanner = new Scanner(System.in);
-        
+        Scanner input = new Scanner(System.in);
+        long inicio = System.nanoTime();
         System.out.println("Digite seu nome:");
-        nome = scanner.nextLine();
+        nome = input .nextLine();
 
         iniciarHistoria();
 
-        if (faseParedes() && fasePortas() && faseRelogio()) {
+        if (faseParedes() && perguntas2() && fasePortas() && perguntas3() && faseRelogio() && perguntas4()) {
             fimJogo();
         }
+        long fim = System.nanoTime();
+        long duracao = fim - inicio;
+        long segundosTotais = duracao / 1_000_000_000;
+        long minutos = segundosTotais / 60;
+        long segundosRestantes = segundosTotais % 60;
+        
+         if (minutos <= 0 && segundosRestantes <30) {
+            System.out.println("Seu tempo: " + minutos + ":" + segundosRestantes + " segundos" );
+            System.out.println("Você não conseguiu ficou preso para sempre!");
+            input
+ .nextLine();
+           } else if (minutos >= 1 && minutos <= 2){
+            System.out.println("Você demorou " + minutos + " minuto e " + segundosRestantes + " e segundos.");
+            System.out.println("Você foi rápido e continua jovem!");  
+            input
+ .nextLine();
+           } else if (minutos >= 2 && minutos <= 5) {
+            System.out.println("Você demorou " + minutos + " minuto e " + segundosRestantes + " e segundos.");
+            System.out.println("Você demorou um pouco e está adulto!");
+            input
+ .nextLine();
+           } else {
+            System.out.println("Você demorou " + minutos + " minuto e " + segundosRestantes + " e segundos.");
+            System.out.println("Você demorou muito e ficou muito velho!");
+            input
+ .nextLine();
 
-        scanner.close();
+         }
+
+        input .close();
     }
 
     public void iniciarHistoria() {
@@ -95,6 +112,20 @@ public class JogoNoiteEstranha {
             'c'
         );
     }
+    ////perguntas2
+    public boolean perguntas2() {
+            return perguntaMultipla(
+            "Qual é o tipo de dado primitivo para representar números inteiros em Java?",
+            new String[]{
+                "a) int",  
+                "b) float",  
+                "c) char",
+                "d) double",
+            },
+            'a'
+        );
+    }
+    
 
     public boolean fasePortas() {
         Scanner input = new Scanner(System.in);
@@ -123,6 +154,20 @@ public class JogoNoiteEstranha {
             'c'
         );
     }
+    
+    public boolean perguntas3() {
+            return perguntaMultipla(
+            "O que significa FOR em um laço de repetição?",
+            new String[]{
+               " a) Definir um tipo de variável",  
+                "b) Declarar uma classe",  
+                "c) Repetir um bloco de código várias vezes ", 
+                "d) Atribuir valores a variáveis",  
+            },
+            'c'
+        );
+    }
+    
 
     public boolean faseRelogio() {
         Scanner input = new Scanner(System.in);
@@ -148,16 +193,30 @@ public class JogoNoiteEstranha {
             'b'
         );
     }
+    
+    public boolean perguntas4() {
+            return perguntaMultipla(
+            "Qual é a estrutura de repetição que verifica a condição após a execução do bloco de código?",
+            new String[]{
+               "a) while",  
+                "b) do-while"  ,
+                "c) FOR"  ,
+                "d) switch" , 
+            },
+            'b'
+        );
+    }
+    
 
     public boolean perguntaMultipla(String pergunta, String[] opcoes, char respostaCorreta) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println(pergunta);
         for (String opcao : opcoes) {
             System.out.println(opcao);
         }
 
         System.out.print("Digite a letra da resposta correta: ");
-        char resposta = scanner.next().toLowerCase().charAt(0);
+        char resposta = input .next().toLowerCase().charAt(0);
 
         switch (resposta) {
             case 'a':
@@ -170,7 +229,8 @@ public class JogoNoiteEstranha {
                 } else {
                     System.out.println("Resposta incorreta. Você falhou!");
                     return false;
-                }
+                    
+                    }
             default:
                 System.out.println("Opção inválida.");
                 return false;
@@ -183,7 +243,20 @@ public class JogoNoiteEstranha {
         System.out.println("pressione enter para continuar..."); 
         input.nextLine();
         System.out.println("De repente, " + nome + " desperta em seu quarto, ofegante e exausto. ");
-        System.out.println(".Olha ao redor, confuso — tudo parece normal, mas a sensação permanece.");
+        System.out.println("Olha ao redor, confuso — tudo parece normal, mas a sensação permanece.");
         System.out.println("Ele se pergunta, com o coração acelerado. Foi apenas um sonho... ou algo mais");
+        input.nextLine();
+        System.out.print("""
+                                                                                                                                
+                               _____      _                     _                      _                              _         
+                              |     |   _| |___ ___ ___ ___ ___| |_ ___ ___    ___ ___| |_ ___ ___    _____ _ _ ___ _| |___ ___ 
+                              |  |  |  | . | -_|_ -| . | -_|  _|  _| .'|  _|  | -_|   |  _|  _| -_|  |     | | |   | . | . |_ -|
+                              |_____|  |___|___|___|  _|___|_| |_| |__,|_|    |___|_|_|_| |_| |___|  |_|_|_|___|_|_|___|___|___|
+                                                   |_|                                                                          
+                              """);
+        input.nextLine();                      
+    }
+}
+
     }
 }
